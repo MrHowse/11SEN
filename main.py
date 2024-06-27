2e934442dba061da7d6aa04646252ca19573b456
Peter Howse <peter.howse@gmail.com>
2024-06-27 12:09:42 +1000

    implementation of new idea

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #!usr/local/bin/python3
 
+def new_idea():
+	print("Yo, new idea here")
+
 
 def main(args=None):
-	print("Hello, world")
+	print("Hello, world. It's good to see you again!")
 	
 
 if __name__ == "__main__":
